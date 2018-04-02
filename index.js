69198f53f2fcb927cd23fd3cc606b9338fa77de1
hendySS <hendysantoso911@gmail.com>
2018-04-02 14:08:18 +0700

    rev1

--- index.js
+++ index.js
@@ -12,12 +12,6 @@ var path = require("path");
 /*mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://List:12345@ds227459.mlab.com:27459/tes');
 */
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
