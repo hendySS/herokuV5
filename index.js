0e5449deb2d2bf77080b9b2140128adaa14803bf
hendySS <hendysantoso911@gmail.com>
2018-04-02 14:18:40 +0700

    v2

--- index.js
+++ index.js
@@ -5,21 +5,27 @@ var port = process.env.PORT || 3000;
 var mongoose = require("mongoose");
 var bodyParse = require("body-parser");
 
-var User = require('./models/account');
+//var User = require('./models/account');
 
-var path = require("path");
+//var path = require("path");
 
 /*mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://List:12345@ds227459.mlab.com:27459/tes');
 */
-
+/*app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+*/
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParse.json());
 app.use(bodyParse.urlencoded({extended : true}));
 //app.use(session({secret:"kajsdkasjdhaskjshsdf", resave:false, saveUninitialized:true}));
 
-var routes = require('./routes/accountRoute');
+//var routes = require('./routes/accountRoute');
 
 routes(app);
 
