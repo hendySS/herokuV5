765acfa660dda0202be8c9e2d50dbfb7b4cea3f3
hendySS <hendysantoso911@gmail.com>
2018-04-02 15:00:51 +0700

    back

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 3000;
 
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname + 'public/index.html'));
+    res.send("Online with heroku");
 });
 
 app.listen(port);
